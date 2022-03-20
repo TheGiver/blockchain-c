b939ae573025fbdfb3618346b4ad977e92a53201
TheGiver <faysalkharraz@gmail.com>
2022-03-20 22:09:25 +0300

    feat: finished implementing the unix-based arraylist utility.

--- blockchain-unix/util/linkedlist/linkedlist.c
+++ blockchain-unix/util/linkedlist/linkedlist.c
@@ -1,17 +1,17 @@
 #include "linkedlist.h"
 
-#define SUCCESS 1; 
-#define FAILURE 0; 
+#define SUCCESS 1 
+#define FAILURE 0
 
 long INITIAL_LENGTH = 10;
-long INCREMENT_BY = 20;
+long RESIZE_BY = 20;
 long currentLength = 0; 
 long currNumOfElements = 0;  
 
 static transaction_t * transactions; 
 
 static bool is_transaction_valid(transaction_t transaction);
-static int realloc_arr();
+static int resize_and_reallocate_data();
 
 int init() {
     transactions = (transaction_t *) calloc(INITIAL_LENGTH, sizeof(transaction_t));
@@ -23,7 +23,8 @@ int init() {
 }
 
 int add(transaction_t transaction) {
-    if(transactions == NULL) {
+    if(transactions == NULL) {  
+        fprintf(stderr, "***************** Error: Failed to add a transaction to the system | Reason: Transactions list not initialized...");
         return FAILURE;
     }
 
@@ -31,13 +32,23 @@ int add(transaction_t transaction) {
         return FAILURE;
     }
 
-    if(currNumOfElements == currentLength/2) {
-        
+    if(currNumOfElements >= currentLength/2) {
+        int status = resize_and_reallocate_data();
+        if(status == FAILURE) {
+            return FAILURE;
+        }
     }
+
+    transactions[currNumOfElements++]=transaction;
+    return SUCCESS;
 }
 
 transaction_t* get_all_transactions() {
-
+    if(transactions == NULL) {  
+        fprintf(stderr, "***************** Error: Failed to add a transaction to the system | Reason: Transactions list not initialized...");
+        return NULL;
+    }
+    return transactions;
 }
 
 long size() {
@@ -46,21 +57,26 @@ long size() {
 
 static bool is_transaction_valid(transaction_t transaction) {
     if(transaction.amount <= 0) {
+        fprintf(stderr, "***************** Error: Invalid transaction | Reason: Transaction amount is less than or equal to zero");
         return false;
     }
 
     if(transaction.receiver == NULL || transaction.sender == NULL) {
+        fprintf(stderr, "***************** Error: Invalid transaction | Reason: Sender or Beneficiary details are invalid");
         return false;
     }
 
     return true;
 }
 
-static int realloc_arr() {
-    transactions = (transaction_t *) realloc(transactions, currentLength+INCREMENT_BY);
-    if(transactions == NULL) {
+static int resize_and_reallocate_data() {
+    currentLength += RESIZE_BY;
+    transaction_t * newRegion = (transaction_t *) realloc(transactions, currentLength);
+    if(newRegion == NULL) {
+        fprintf(stderr, "***************** Error: Failed to resize the list | Reason: Memory reallocation failed");
         return FAILURE;
     }
+    memcpy(newRegion, transactions, currNumOfElements*sizeof(transaction_t));
+    transactions = newRegion;
     return SUCCESS;
 }
-
